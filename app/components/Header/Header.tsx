e7dc35f771a287900e7a937f3169bc5732b84ce4
Lucas S <lucasluke307@gmail.com>
2025-03-17 09:28:29 -0300

    Adding files to the repository

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import './Header.css';
+
+import { useCallback, useState } from 'react';
+
+export default function Header() {
+    const logotipo = "/visual_identity/burguer-mania-logo.png";
+    const navLinks = [
+        { title: "Home", link: "#" },
+        { title: "Categorias", link: "#category" },
+        { title: "Cardápio", link: "#menu" },
+        { title: "Localização", link: "#localization" },
+        { title: "Contato", link: "#contact" },
+    ];
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+    const toggleMenu = useCallback(() => {
+        setMenuIsOpen((prev) => !prev);
+    }, []);
+
+    return (
+        <nav className="nav-container">
+            <div className="nav-content">
+                <a href="#" className="brand-box">
+                    <img src={logotipo} alt="Burguer Mania" className="h-8" />
+                </a>
+
+                <button
+                    type="button"
+                    onClick={toggleMenu}
+                    data-collapse-toggle="navbar"
+                    className="burger-menu-button"
+                    aria-controls="navbar"
+                    aria-expanded={menuIsOpen ? "true" : "false"}
+                >
+                    <i className="bi bi-list text-black"></i>
+                </button>
+
+                <div className={`${menuIsOpen ? "block" : "hidden"} nav-items-box`} id="navbar">
+                    <ul className="nav-list">
+                        {navLinks.map((navItem, index) => (
+                            <li key={index}>
+                                <a href={navItem.link} className="nav-item">
+                                    {navItem.title}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
